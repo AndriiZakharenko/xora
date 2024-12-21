be5d2f30fc9f0682cf72446d52f4eee93593565f
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-12-21 14:39:08 +0200

    feat: updated header, navbar bg

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -3,14 +3,17 @@ import { Link as LinkScroll } from "react-scroll";
 interface NavLinkProps {
   title: string;
   to: string;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const NavLink = ({ title, to }: NavLinkProps) => (
+const NavLink = ({ title, to, setIsOpen }: NavLinkProps) => (
   <LinkScroll
+    onClick={() => setIsOpen(false)}
     to={to}
     offset={-100}
     spy
     smooth
+    activeClass="nav-active"
     className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
   >
     {title}
