34b6c33517e36804f4f9e41e6770d41b171f5064
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-12-21 22:45:15 +0200

    feat: FAQ section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Fag from "./sections/Faq";
 import Features from "./sections/Features";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <Features />
       <Pricing/>
+      <Fag/>
     </main>
   );
 };
