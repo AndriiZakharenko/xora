2c8230838df483f96307c7791454300a2eb97fe3
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-12-22 17:59:38 +0200

    feat: Downloads

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Download from "./sections/Download";
 import Fag from "./sections/Faq";
 import Features from "./sections/Features";
 import Header from "./sections/Header";
@@ -14,6 +15,7 @@ const App = () => {
       <Pricing/>
       <Fag/>
       <Testimonials/>
+      <Download/>
     </main>
   );
 };
